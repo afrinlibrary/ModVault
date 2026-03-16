2f91b1ec623b6d5af6c6c59132e2a54c3be7c480
MaxJubayerYT <maxjubayergaming@gmail.com>
2026-03-16 05:19:46 +0000

    Remove AppCompatDelegate night mode call to fix startup crash

--- app/src/main/java/com/modvault/app/MainActivity.java
+++ app/src/main/java/com/modvault/app/MainActivity.java
@@ -90,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode(androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
